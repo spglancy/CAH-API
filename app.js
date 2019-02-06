2901f255494049cb97ccfa15cd17ce877b6ab11b
Sean <Spglancy@gmail.com>
2019-02-06 13:02:18 -0800

    init

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json(a)
+})
+
+app.listen(3000, () => {
+    console.log(`App running on port 3000`)
+})

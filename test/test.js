70fa0cfc9c8fb138f3edbde6666e1d2d5484d6b5
Sean <Spglancy@gmail.com>
2019-02-27 14:46:25 -0800

    STYLEGUIDES PLS

--- test/test.js
+++ test/test.js
@@ -1,29 +1,34 @@
+/* eslint-disable semi */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable no-undef */
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app');
-const should = chai.should();
 
-
-chai.should()
-chai.use(chaiHttp);
+// const should = chai.should()
+chai.use(chaiHttp)
 
 
 describe('CardSet', function () {
-
   after(() => {
     
   })
-    
-  it("Should return a collection of cards GET", function() {
-      chai.request(server)
-      .get("/sets/base")
+     
+  it('Should return a collection of cards GET', function () {
+    chai.request(server)
+      .get('/sets/base')
       .end((err, res) => {
-          res.should.have.status(200)
-          done()
+        res.should.have.status(200)
+        done()
       })
   })
 
-  it("Should GET a collection of X cards from a Y cardset")
-  it("Should POST card to a proposed card object")
+  it('Should GET a collection of X cards from a Y cardset', function () {
+
+  })
+  it('Should POST card to a proposed card object', function () {
+
+  })
 
 })
